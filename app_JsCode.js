1435e1bae1737269732451cb5de7c847ba291226
Ankit Mittal <ankitmittal524@gmail.com>
2023-04-09 13:49:44 +0530

    new File added

---
+++ app_JsCode.js
@@ -0,0 +1,50 @@
+
+var i = 5;
+var b = 11;
+
+function abc() {
+    var h = 21;
+    i = 11;
+
+    var lmn = function () {
+        // b = 21;
+        // h = 99;
+    }
+
+    // function lmn() {
+    //     // ijk();
+    //     // b = 21;
+    //     // h = 99;
+    // }
+
+    // var ijk = function () {
+    //     b = 21;
+    //     // h = 99;
+    // }
+
+    function ijk() {
+        // var u = 55;
+        // function mno() {
+        //     u = 21;
+        // }
+        // return mno;
+        // return lmn;
+        // lmn();
+        // b = 21;
+        // h = 99;
+    }
+
+    // abc.ijk = function () {
+
+    // }
+
+    // return ijk;
+    return lmn;
+    // return abc.ijk;
+    // return function ijk() {
+    //     b = 21;
+    //     h = 99;
+    // }
+}
+// abc()();
+// console.log(b);
